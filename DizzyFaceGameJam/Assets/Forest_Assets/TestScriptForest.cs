ce95e0a9ef0815341b29f857971714b3e1cabcef
forestwwww <altforowban5@gmail.com>
2025-10-10 21:25:03 -0700

    test push
    testest

---
+++ DizzyFaceGameJam/Assets/Forest_Assets/TestScriptForest.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class TestScriptForest : MonoBehaviour
+{
+    public TMP_Text thisText; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        thisText.text = "TestText";
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
